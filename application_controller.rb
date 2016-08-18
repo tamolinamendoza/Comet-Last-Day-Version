6fe0acaa9b67d8e287a8f16db1333862b1870020
eitanz123 <eitan@darwish.com>
2016-08-18 18:04:00 +0000

    functional dropdown with no aesthetics

--- application_controller.rb
+++ application_controller.rb
@@ -1,9 +1,28 @@
 require 'bundler'
 Bundler.require
+#require_relative 'models/connect.rb' #need to change the model route/ruby file when combining w/ sam's ruby
 require_relative 'models/model.rb'
 
 class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+get '/personalized' do
+  erb :index
+end
+
+  post '/personalized' do
+    #binding.pry
+    @neighborhood = params[:neighborhood]
+    @price = params[:price]
+    @gender = params[:gender]
+
+    @preference = stores(@neighborhood, @price, @gender)
+   # binding.pry
+
+    erb :page2
+  end
+
+
 end
